fe139817d70dc9884cfc68a040591b85daffc548
ihalvor <240202@usn.no>
2022-10-26 13:55:59 +0200

    Changes by eirik

--- app/src/main/java/com/example/memorutest1/MainActivity.java
+++ app/src/main/java/com/example/memorutest1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.memorutest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.btn_new_item).setOnClickListener(view -> {
+            startActivity(new Intent(this, ActivityAddItem.class));
+        });
+
+        findViewById(R.id.btn_my_item).setOnClickListener(view -> {
+            startActivity(new Intent(this, ActivityMyItems.class));
+        });
+
+        findViewById(R.id.btn_settings).setOnClickListener(view -> {
+            startActivity(new Intent(this, ActivitySettings.class));
+        });
     }
 }
