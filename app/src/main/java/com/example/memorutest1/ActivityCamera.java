295b4810f4abc0f5cc1015ccea09cc0bf99c382b
ihalvor <240202@usn.no>
2022-10-26 13:48:51 +0200

    Start of design

---
+++ app/src/main/java/com/example/memorutest1/ActivityCamera.java
@@ -0,0 +1,14 @@
+package com.example.memorutest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityCamera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
---
+++ app/src/main/java/com/example/memorutest1/ActivityLogIn.java
@@ -0,0 +1,14 @@
+package com.example.memorutest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
