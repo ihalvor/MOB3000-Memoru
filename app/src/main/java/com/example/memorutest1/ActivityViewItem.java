4520f626ef1b455e0ee41e492b325fc9316bb22c
Eiriksm0raas <eirik.sm0raas@gmail.com>
2022-11-15 13:40:30 +0100

    Added item view for viewing specific items

--- app/src/main/java/com/example/memorutest1/ActivityViewItem.java
+++ app/src/main/java/com/example/memorutest1/ActivityViewItem.java
@@ -2,13 +2,78 @@ package com.example.memorutest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.squareup.picasso.Picasso;
+
+import java.util.Map;
 
 public class ActivityViewItem extends AppCompatActivity {
+
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user == null) finish();
+
+        findViewById(R.id.txt_back).setOnClickListener((View view) -> finish());
+
+        // Download and load item data
+        Database database = Database.getInstance();
+        String itemID = getIntent().getStringExtra("itemID");
+        String userID = user.getUid();
+
+        findViewById(R.id.btn_edit).setOnClickListener((View view) -> {
+            startActivity(new Intent(this, ActivityAddItem.class)
+                    .putExtra("itemID", itemID));
+        });
+
+        database.downloadUserItem(userID, itemID)
+                .addOnSuccessListener((DocumentSnapshot snapshot) -> {
+                    Map<String, Object> item = snapshot.getData();
+
+                    database.downloadImage(Database.findImageAddress(
+                            userID, itemID, Database.ImageType.ITEM))
+                            .addOnSuccessListener((Uri imageUri) -> {
+                                displayItemData(item, imageUri);
+                            })
+                            .addOnFailureListener((Exception e) -> {
+                                Toast.makeText(this,
+                                        "No image found",
+                                        Toast.LENGTH_SHORT).show();
+                            });
+
+                })
+                .addOnFailureListener((Exception e) -> {
+                    Toast.makeText(this, "No item found", Toast.LENGTH_SHORT).show();
+                });
+
+    }
+
+    private void displayItemData(Map<String, Object> item, Uri imageUri) {
+
+        String name         = item.get("name").toString();
+        String location     = item.get("location").toString();
+        String description  = item.get("description").toString();
+
+        ImageView imageView = findViewById(R.id.img_my_image);
+        Picasso.get().load(imageUri).into(imageView);
+
+        ((TextView) findViewById(R.id.txt_name)).setText(name);
+        ((TextView) findViewById(R.id.txt_location)).setText(location);
+        ((TextView) findViewById(R.id.txt_desc)).setText(description);
     }
 }
