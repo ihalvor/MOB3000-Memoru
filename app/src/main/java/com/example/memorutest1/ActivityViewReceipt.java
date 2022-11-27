36c9a54065f207874c456bdbcc1531de2e0cc1a2
Eiriksm0raas <eirik.sm0raas@gmail.com>
2022-11-27 18:57:02 +0100

    Added receiptView, a view to see a full size image of the receipt

---
+++ app/src/main/java/com/example/memorutest1/ActivityViewReceipt.java
@@ -0,0 +1,43 @@
+package com.example.memorutest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+public class ActivityViewReceipt extends AppCompatActivity {
+
+    public static final String EXTRA_URI_KEY = "EXTRA_URI_KEY";
+    public static final String EXTRA_ITEM_NAME = "EXTRA_ITEM_NAME";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_receipt);
+
+        findViewById(R.id.txt_back).setOnClickListener((View view) -> finish());
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(EXTRA_ITEM_NAME);
+        Uri uri = intent.getParcelableExtra(EXTRA_URI_KEY);
+
+        if(name == null || uri == null) {
+            Toast.makeText(this, "Could not view receipt", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        ImageView imageView = findViewById(R.id.img_my_receipt);
+        Picasso.get().load(uri).into(imageView);
+
+        ((TextView) findViewById(R.id.txt_name)).setText(name + "'s receipt");
+    }
+}
