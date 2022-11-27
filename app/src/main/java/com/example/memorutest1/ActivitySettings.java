ae208c9fd502cb443145eb91d78e5df4f2788f1d
Eiriksm0raas <eirik.sm0raas@gmail.com>
2022-11-27 12:59:59 +0100

    nothing

--- app/src/main/java/com/example/memorutest1/ActivitySettings.java
+++ app/src/main/java/com/example/memorutest1/ActivitySettings.java
@@ -2,7 +2,9 @@ package com.example.memorutest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivitySettings extends AppCompatActivity {
 
