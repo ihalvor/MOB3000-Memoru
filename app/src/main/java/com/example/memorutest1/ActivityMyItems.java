889b22ff277c72556cf82d1d877d399968dbd165
Eiriksm0raas <eirik.sm0raas@gmail.com>
2022-11-30 15:45:12 +0100

    Items will now render with favourite icon according to if the item is "favourited"

--- app/src/main/java/com/example/memorutest1/ActivityMyItems.java
+++ app/src/main/java/com/example/memorutest1/ActivityMyItems.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -83,7 +84,20 @@ public class ActivityMyItems extends AppCompatActivity {
     }
 
     private View getItemDisplay(Map<String, Object> item, Task<Uri> task, String itemID) {
-        View layout = getLayoutInflater().inflate(R.layout.my_item, null);
+        boolean favourite;
+        try {
+            favourite = item.get("fav").toString() == "true";
+        } catch (Exception e) {
+            favourite = false;
+        }
+
+        // For older APIs we can not load the fav icon into an imageView,
+        // we therefor have two different versions of my_item
+        View layout = getLayoutInflater().inflate(
+                favourite
+                    ? R.layout.my_item_fav
+                    : R.layout.my_item,
+                null);
 
         ImageView imageView = layout.findViewById(R.id.mini_image);
         if(task.isSuccessful()) {
@@ -96,6 +110,29 @@ public class ActivityMyItems extends AppCompatActivity {
 
         ((TextView) layout.findViewById(R.id.txt_name)).setText(item.get("name").toString());
         ((TextView) layout.findViewById(R.id.txt_desc)).setText(item.get("description").toString());
+
+        boolean[] favArr = { favourite };
+        layout.findViewById(R.id.img_star).setOnClickListener((View view) -> {
+            // Create local reference to this lambda
+            favArr[0] = !favArr[0];
+            boolean isFav = favArr[0];
+
+            // Update database
+            Database.getInstance().favItem(user.getUid(), itemID, isFav);
+
+            // Update icon when pressed
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ((ImageView) view).setImageIcon(Icon.createWithResource(
+                        getApplicationContext(),
+                        isFav
+                            ? R.drawable.ic_baseline_star_24
+                            : R.drawable.ic_baseline_star_border_24));
+            } else {
+                // For older APIs, refresh the page to show correct favourite icon
+                startActivity(new Intent(this, ActivityMyItems.class));
+                finish();
+            }
+        });
 
         layout.setOnClickListener((View view) -> {
             startActivity(new Intent(this, ActivityViewItem.class)
