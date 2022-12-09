6e9bd9768bf40d861084ccec67e31a91f52a39e9
halvor <halvoras97@hotmail.com>
2022-12-09 18:06:25 +0100

    Changes by eirik

--- app/src/main/java/com/example/memorutest1/ActivityMyItems.java
+++ app/src/main/java/com/example/memorutest1/ActivityMyItems.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.drawable.Icon;
 import android.net.Uri;
@@ -12,8 +13,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,7 +39,13 @@ import java.util.Map;
 
 public class ActivityMyItems extends AppCompatActivity {
 
-    FirebaseUser user;
+    public static final String TAG = "MY_ITEMS";
+    private FirebaseUser user;
+    private ArrayList<MyItem> myItems = new ArrayList<>();
+    private ArrayList<MyItem> filteredItems = new ArrayList<>();
+
+    private ItemAdapter itemAdapter = new ItemAdapter(myItems, filteredItems);
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +71,20 @@ public class ActivityMyItems extends AppCompatActivity {
         Database database = Database.getInstance();
         database.downloadUserItems(user.getUid()).addOnCompleteListener((Task<QuerySnapshot> task) -> {
             if (task.isSuccessful()) {
+
+                listView = findViewById(R.id.layout_scroll);
+                listView.setAdapter(itemAdapter);
+
                 for (QueryDocumentSnapshot document : task.getResult()) {
                     Map<String, Object> item = document.getData();
 
                     String itemID = document.getId();
                     String userID = user.getUid();
 
-                    ((LinearLayout) findViewById(R.id.layout_scroll))
-                            .addView(getItemDisplay(item, userID, itemID));
+                    MyItem myItem = new MyItem(item, itemID, userID, null, null);
+
+                    filteredItems.add(myItem);
+                    myItems.add(myItem);
                 }
             } else {
                 Toast.makeText(this, "Could not load items", Toast.LENGTH_SHORT).show();
@@ -75,6 +94,91 @@ public class ActivityMyItems extends AppCompatActivity {
         .addOnFailureListener((Exception e) -> {
             Log.e("MY_ITEMS", e.toString());
         });
+
+        SearchView search = findViewById(R.id.search);
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                itemAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+    }
+
+
+    public class ItemAdapter extends BaseAdapter implements Filterable {
+        @Override
+        public Filter getFilter() {
+            return filter;
+        }
+
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                ArrayList<MyItem> filteredList = new ArrayList<>();
+
+                if(charSequence.toString().isEmpty()) {
+                    filteredList.addAll(myItems);
+                } else {
+                    for(MyItem item : myItems) {
+                        if(item.toString().toLowerCase().contains(charSequence.toString().toLowerCase())) {
+                            filteredList.add(item);
+                        }
+                    }
+                }
+
+                FilterResults result = new FilterResults();
+                result.values = filteredList;
+
+                return result;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredItems.clear();
+                filteredItems.addAll((Collection<? extends MyItem>) filterResults.values);
+                notifyDataSetChanged();
+            }
+        };
+
+        private ArrayList<MyItem> myItems;
+        private ArrayList<MyItem> filteredItems;
+
+        public ItemAdapter(ArrayList<MyItem> myItems, ArrayList<MyItem> filteredItems) {
+            this.myItems = myItems;
+            this.filteredItems = filteredItems;
+        }
+
+        @Override
+        public int getCount() { return filteredItems.size(); }
+
+        @Override
+        public Object getItem(int i) { return filteredItems.get(i); }
+
+        @Override
+        public long getItemId(int i) { return 0; }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            MyItem item = filteredItems.get(i);
+
+            if(view == null) {
+                return getItemDisplay(item.getMap(), item.getUserID(), item.getItemID(), null);
+            } else {
+                getItemDisplay(item.getMap(), item.getUserID(), item.getItemID(), view);
+            }
+
+            return view;
+        }
+
     }
 
     @Override
@@ -86,7 +190,7 @@ public class ActivityMyItems extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private View getItemDisplay(Map<String, Object> item, String userID, String itemID) {
+    private View getItemDisplay(Map<String, Object> item, String userID, String itemID, View view) {
         boolean favourite;
         try {
             favourite = item.get("fav").toString() == "true";
@@ -96,11 +200,17 @@ public class ActivityMyItems extends AppCompatActivity {
 
         // For older APIs we can not load the fav icon into an imageView,
         // we therefor have two different versions of my_item
-        View layout = getLayoutInflater().inflate(
-                favourite
-                    ? R.layout.my_item_fav
-                    : R.layout.my_item,
-                null);
+        View layout;
+
+        if(view != null) {
+            layout = view;
+        } else {
+            layout = getLayoutInflater().inflate(
+                    favourite
+                            ? R.layout.my_item_fav
+                            : R.layout.my_item,
+                    null);
+        }
 
         // Download and display image if item has one
         ImageView imageView = layout.findViewById(R.id.mini_image);
@@ -115,10 +225,11 @@ public class ActivityMyItems extends AppCompatActivity {
                 });
 
         ((TextView) layout.findViewById(R.id.txt_name)).setText(item.get("name").toString());
-        ((TextView) layout.findViewById(R.id.txt_desc)).setText(item.get("description").toString());
+        ((TextView) layout.findViewById(R.id.txt_location)).setText(item.get("location").toString());
 
         boolean[] favArr = { favourite };
-        layout.findViewById(R.id.img_star).setOnClickListener((View view) -> {
+        layout.findViewById(R.id.img_star).setOnClickListener((View innerView) -> {
+
             // Create local reference to this lambda
             favArr[0] = !favArr[0];
             boolean isFav = favArr[0];
@@ -128,7 +239,7 @@ public class ActivityMyItems extends AppCompatActivity {
 
             // Update icon when pressed
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                ((ImageView) view).setImageIcon(Icon.createWithResource(
+                ((ImageView) innerView).setImageIcon(Icon.createWithResource(
                         getApplicationContext(),
                         isFav
                             ? R.drawable.ic_baseline_star_24
@@ -140,7 +251,8 @@ public class ActivityMyItems extends AppCompatActivity {
             }
         });
 
-        layout.setOnClickListener((View view) -> {
+        layout.setOnClickListener((View innerView) -> {
+
             startActivity(new Intent(this, ActivityViewItem.class)
                     .putExtra("itemID", itemID));
         });
